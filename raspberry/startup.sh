bf17843fd6634658cab250c97e6e385fe401ef6a
Ivan Chernov <ichernov@c-cars.tech>
2022-01-20 22:25:56 +0300

    Fix deploy again

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
-git clone https://github.com/vanya-black/my-air-app.git
 sudo cp /home/pi/my-air-app/raspberry/air_master.service /etc/systemd/system/air_master.service
 sudo systemctl daemon-reload
 sudo systemctl enable air_master.service
