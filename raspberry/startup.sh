16324cab01db6f60bf9638a404d202a67c32ef31
Ivan Chernov <ichernov@c-cars.tech>
2022-01-21 23:35:24 +0300

    Fix enviroment var for systemd

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 echo "Start script started"
-pip install -r /home/pi/my-air-app/requirements.txt
-
-echo "$1 $2 $3 $4" > test.txt
+pip install -r /home/pi/my-air-app/requirements.txtsystemctl show-environment
 
 sudo cp /home/pi/my-air-app/raspberry/air_master.service /etc/systemd/system/air_master.service
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.service /etc/systemd/system/sync_to_s3.service
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.timer /etc/systemd/system/sync_to_s3.timer
+
+touch /etc/systemd/system/sync_to_s3.service.d/override.conf
+echo "[Service]" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+echo "Enviroment=S3_Host=$1" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+echo "Enviroment=S3_ACCESS_KEY=$2" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+echo "Enviroment=S3_SECRET_KEY=$3" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+echo "Enviroment=S3_BUCKET=$4" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+
 sudo systemctl daemon-reload
 sudo systemctl enable air_master.service
 sudo systemctl restart air_master.service
