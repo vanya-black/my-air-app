388319ec6d40cf2a06bad48f487e9bd543f60afa
Ivan Chernov <ichernov@c-cars.tech>
2022-01-21 12:31:39 +0300

    Fix path

--- raspberry/air_master.py
+++ raspberry/air_master.py
@@ -8,10 +8,10 @@ import sqlite3
 from pathlib import Path
 
 COMMAND = b'\x55\xCD\x47\x00\x00\x00\x00\x00\x00\x01\x69\x0D\x0A'
-DB_NAME = Path(Path.home(), 'my-air-app', 'air.db')
+DB_NAME = Path(Path.home(), 'air.db')
 REQUEST_INTERVAL = 5
 
-logging.basicConfig(filename=Path(Path.home(), 'my-air-app', 'air_maste.log'), encoding='utf-8', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(filename=Path(Path.home(), 'air_maste.log'), encoding='utf-8', level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
 def get_connection() -> serial.Serial:
     is_connected = False
