6c4173f0a608605e8e759e4a142f363c09816ae9
Ivan Chernov <ichernov@c-cars.tech>
2022-01-21 21:00:49 +0300

    Try to export test env var

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 pip install -r /home/pi/my-air-app/requirements.txt
 
+export TEST=666
+
 sudo cp /home/pi/my-air-app/raspberry/air_master.service /etc/systemd/system/air_master.service
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.service /etc/systemd/system/sync_to_s3.service
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.timer /etc/systemd/system/sync_to_s3.timer
