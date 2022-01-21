2b3d13e60007c4248211767116ebab0ee3ac5f37
Ivan Chernov <ichernov@c-cars.tech>
2022-01-21 23:46:37 +0300

    add sudo permission

--- raspberry/startup.sh
+++ raspberry/startup.sh
@@ -6,13 +6,13 @@ sudo cp /home/pi/my-air-app/raspberry/air_master.service /etc/systemd/system/air
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.service /etc/systemd/system/sync_to_s3.service
 sudo cp /home/pi/my-air-app/raspberry/sync_to_s3.timer /etc/systemd/system/sync_to_s3.timer
 
-mkdir -p /etc/systemd/system/sync_to_s3.service.d
-touch /etc/systemd/system/sync_to_s3.service.d/override.conf
-echo "[Service]" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
-echo "Environment=S3_Host=$1" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
-echo "Environment=S3_ACCESS_KEY=$2" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
-echo "Environment=S3_SECRET_KEY=$3" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
-echo "Environment=S3_BUCKET=$4" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo mkdir -p /etc/systemd/system/sync_to_s3.service.d
+sudo echo -n "" > /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo echo "[Service]" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo echo "Environment=S3_Host=$1" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo echo "Environment=S3_ACCESS_KEY=$2" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo echo "Environment=S3_SECRET_KEY=$3" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
+sudo echo "Environment=S3_BUCKET=$4" >> /etc/systemd/system/sync_to_s3.service.d/override.conf
 
 sudo systemctl daemon-reload
 sudo systemctl enable air_master.service
